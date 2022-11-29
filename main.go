56114f7f1440265a891e1128e733e0da54a88e85
Rain Wu <rain.wu@appier.com>
2022-11-29 21:00:05 +0800

    feat($atlas): add ignore flag
    Signed-off-by: Rain Wu <rain.wu@appier.com>

--- main.go
+++ main.go
@@ -7,10 +7,27 @@ import (
 	"telescope/telescope"
 )
 
+type IgnoredDependencies map[string]bool
+
+func (i *IgnoredDependencies) String() string {
+
+	var ignored []string
+	for dep := range *i {
+		ignored = append(ignored, dep)
+	}
+	return fmt.Sprintln(ignored)
+}
+
+func (i *IgnoredDependencies) Set(value string) error {
+	map[string]bool(*i)[value] = true
+	return nil
+}
+
 var (
-	filePath      string
-	outdatedScope string
-	skipUnknown   bool
+	filePath            string
+	outdatedScope       string
+	skipUnknown         bool
+	ignoredDependencies IgnoredDependencies = make(map[string]bool)
 )
 
 func init() {
@@ -18,11 +35,12 @@ func init() {
 	flag.StringVar(&filePath, "f", "go.mod", "dependencies file path")
 	flag.StringVar(&outdatedScope, "s", "major", "desired outdated scope")
 	flag.BoolVar(&skipUnknown, "skip-unknown", false, "skip dependencies with unknown versions")
+	flag.Var(&ignoredDependencies, "i", "ignore specific dependency")
 	flag.Usage = usage
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: telescope [-f file_path] [-s outdated_scope] [--skip-unknown]\n")
+	fmt.Fprintf(os.Stderr, "Usage: telescope [-f file_path] [-s outdated_scope] [-i ignored_dependency] [--skip-unknown]\n")
 	flag.PrintDefaults()
 }
 
@@ -30,7 +48,7 @@ func main() {
 
 	flag.Parse()
 
-	atlas := telescope.NewAtlas(filePath)
+	atlas := telescope.NewAtlas(filePath, ignoredDependencies)
 	atlas.ReportOutdated(
 		telescope.OutdatedScopeStrToEnum(outdatedScope),
 		skipUnknown,
